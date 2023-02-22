8a8a13292b8ea30a327d889f519296a98ed5daa5
JAY LAKHANI <88767949+20IT062@users.noreply.github.com>
2023-02-22 23:08:47 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello AWS!
+          Hello AWS! Jay lakhani
         </p>
         <a
           className="App-link"
