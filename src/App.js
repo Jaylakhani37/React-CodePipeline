1a277dc2c93dd3361e61b407759fb53facdd40b2
Jaylakhani37 <20it062@charusat.edu.in>
2023-03-01 20:36:32 +0530

    Done Update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Project Vandan
+          Learn Project Raj Belu
         </a>
       </header>
     </div>
